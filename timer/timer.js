c4352a191230a3b0e7167f6a231d82c1f36e5d2d
Prncy <princelay1313@gmail.com>
2026-09-07 08:09:16 +0200

    Update timer.js

--- timer/timer.js
+++ timer/timer.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Target Time: 2nd September 2026 at 15:30 PM
-  const targetDate = new Date("2026-09-02T15:30:00").getTime();
+  const targetDate = new Date("2026-09-10T10:00:00").getTime();
 
   const units = {
     days: document.querySelector('[data-unit="days"]'),
